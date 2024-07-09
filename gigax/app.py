9bec379dc176cbe48533fef34873d0720689b828
Tristan <tr.deborde@gmail.com>
2024-07-09 14:17:23 +0200

    Simple route to init model, or quantize it with llama-cpp-python

--- gigax/app.py
+++ gigax/app.py
@@ -2,16 +2,19 @@ import logging
 import os
 import uvicorn
 import sys
+import llama_cpp
 
 from dotenv import load_dotenv
+from llama_cpp import Llama, llama_model_quantize_params
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Request, status
+from outlines import models
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.logger import logger as fastapi_logger
 from pydantic import BaseModel
 
-from gigax.parse import CharacterAction, ProtagonistCharacter
+from gigax.parse import CharacterAction, NarratorUpdate, ProtagonistCharacter
 from gigax.scene import (
     Character,
     Item,
@@ -59,6 +62,37 @@ class CharacterActionRequest(BaseModel):
     protagonist: ProtagonistCharacter
     items: list[Item]
     events: list[CharacterAction]
+
+
+@app.get("/init_model/", response_description="Initialize the model")
+async def init_model(is_local: bool = False):
+    global stepper
+    if is_local:
+        # llm = Llama.from_pretrained(
+        #     repo_id="Gigax/NPC-LLM-3_8B-GGUF",  # path to GGUF file
+        #     filename="npc-llm-3_8B_Q4_1_low_level.gguf",
+        #     local_dir="./models",
+        #     n_ctx=4096,  # The max sequence length to use - note that longer sequence lengths require much more resources
+        #     n_threads=8,  # The number of CPU threads to use, tailor to your system and the resulting performance
+        #     n_gpu_layers=35,  # The number of layers to offload to GPU, if you have GPU acceleration available. Set to 0 if no GPU acceleration is available on your system.
+        # )
+        llm = Llama(
+            model_path="models/npc-llm-3_8B_Q4_1_low_level.gguf",
+            n_ctx=4096,
+            n_threads=8,
+            n_gpu_layers=35,
+        )
+        model = models.LlamaCpp(llm)  # type: ignore
+        # result = llama_cpp.llama_model_quantize(
+        #     "models/npc-llm-3_8B.gguf".encode("utf-8"),
+        #     "models/npc-llm-3_8B_Q4_1_low_level.gguf".encode("utf-8"),
+        #     llama_model_quantize_params(0, 3, True, True, False),
+        # )
+        # exit()
+    else:
+        model = "llama_3_regex"
+
+    stepper = NPCStepper(model=model, api_key=os.getenv("API_KEY"))
 
 
 @app.post(
